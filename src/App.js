819b6a5b9ede703dafd74268ce2b1ad7878ac545
Sam Wasden <samwasden@outlook.com>
2021-09-25 21:45:18 -0600

    final

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./App.css";
+import NewTask from "./NewTask";
+import List from "./List";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: []
+    };
+
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  handleAddTask(task) {
+    this.setState({ list: [...this.state.list, task] });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My to-do list:</h1>
+        <NewTask hello={this.handleAddTask} />
+        <List tasks={this.state.list} />
+      </div>
+    );
+  }
+}
+
+export default App;
